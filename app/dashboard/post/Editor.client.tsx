cef574807243ed1274af3556cb30b6c4daa680a5
zerozoo <zerozoo385@gmail.com>
2023-10-09 01:17:33 +0900

    add title

--- app/dashboard/post/Editor.client.tsx
+++ app/dashboard/post/Editor.client.tsx
@@ -1,10 +1,11 @@
 "use client";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor } from "@toast-ui/react-editor";
-import { createRef } from "react";
+import { ChangeEvent, createRef, useState } from "react";
 
 export default function Post() {
   const editorRef = createRef<any>();
+  const [title, setTitle] = useState<string>("");
   const toolbarItems = [
     ["heading", "bold", "italic", "strike"],
     ["hr", "quote"],
@@ -13,13 +14,43 @@ export default function Post() {
     ["scrollSync"],
   ];
 
-  function handleOnClick() {
+  async function handleOnClick() {
     const instance = editorRef.current.getInstance();
-    const html = instance.getHTML();
+    const context = instance.getHTML();
+    if (validateValues({ title, context })) return;
+  }
+
+  function validateValues({
+    title,
+    context,
+  }: {
+    title?: string | void;
+    context?: string;
+  }) {
+    if (!title) {
+      alert("제목을 입력해주세요");
+      return false;
+    }
+    if (title.length < 3) {
+      alert("제목의 길이는 3자 이상으로 제한됩니다.");
+      return false;
+    }
+
+    if (!context) {
+      alert("본문을 입력해주세요");
+      return false;
+    }
+
+    if (title && context) return true;
+  }
+
+  function handleTitleOnChange(event: ChangeEvent<HTMLInputElement>) {
+    setTitle(event.target.value);
   }
 
   return (
-    <>
+    <div className="p-4">
+      <TitleInput handleTitleOnChange={handleTitleOnChange} />
       <Editor
         // @ts-ignore
         ref={editorRef}
@@ -35,6 +66,23 @@ export default function Post() {
         initialValue=" "
       />
       <button onClick={handleOnClick}>글쓰기</button>
-    </>
+    </div>
+  );
+}
+
+function TitleInput({ handleTitleOnChange }) {
+  return (
+    <div className="py-4">
+      <label htmlFor="title" className="text-gray-600">
+        제목:
+      </label>
+      <input
+        type="text"
+        id="title"
+        className="border rounded-lg px-3 py-2 w-full"
+        placeholder="Enter a title"
+        onChange={(e) => handleTitleOnChange(e)}
+      />
+    </div>
   );
 }
