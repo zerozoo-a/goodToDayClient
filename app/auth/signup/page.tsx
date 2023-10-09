f98444048d56fba299cbc5809ba05661ef822b16
zerozoo <zerozoo385@gmail.com>
2023-10-10 00:15:14 +0900

    house sign up api add

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,19 +1,29 @@
 export default function SignUp() {
   async function create(formData: FormData) {
     "use server";
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const confirm_password = formData.get("confirm_password");
+
+    // email check || username check from db
+
+    // password === confirm_password check
+
+    // save db
   }
 
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
-        <h2 className="text-2xl font-semibold mb-4">User Signup</h2>
+        <h2 className="text-2xl font-semibold mb-4">회원가입</h2>
         <form action={create} method="POST">
           <div className="mb-4">
             <label
               htmlFor="username"
               className="block text-sm font-medium text-gray-700"
             >
-              Username:
+              닉네임:
             </label>
             <input
               type="text"
@@ -28,7 +38,7 @@ export default function SignUp() {
               htmlFor="email"
               className="block text-sm font-medium text-gray-700"
             >
-              Email:
+              E-mail:
             </label>
             <input
               type="email"
@@ -43,7 +53,7 @@ export default function SignUp() {
               htmlFor="password"
               className="block text-sm font-medium text-gray-700"
             >
-              Password:
+              비밀번호:
             </label>
             <input
               type="password"
@@ -58,7 +68,7 @@ export default function SignUp() {
               htmlFor="confirm_password"
               className="block text-sm font-medium text-gray-700"
             >
-              Confirm Password:
+              비밀번호 확인:
             </label>
             <input
               type="password"
@@ -71,9 +81,9 @@ export default function SignUp() {
           <div className="mt-6">
             <button
               type="submit"
-              className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700"
+              className="px-4 py-2 bg-teal-600 text-white rounded hover:bg-indigo-700"
             >
-              Sign Up
+              회원가입
             </button>
           </div>
         </form>
