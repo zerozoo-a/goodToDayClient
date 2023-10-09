9faaf03c6646d90712b00ce8bb9c22c4c3029683
zerozoo <zerozoo385@gmail.com>
2023-10-10 01:37:46 +0900

    add validate user

--- app/auth/signup/actions/createUser.ts
+++ app/auth/signup/actions/createUser.ts
@@ -5,53 +5,36 @@ export async function createUser(formData: FormData) {
   //   const password = formData.get("password");
   //   const confirm_password = formData.get("confirm_password");
 
-  //   console.log("🚀 ~ file: page.tsx:11 ~ create ~ url:", url);
-
-  const responseOfIsValidEmail = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER}users/findBy/email`,
-    {
+  const [responseOfIsValidEmail, responseOfIsValidName] = await Promise.all([
+    fetch(`${process.env.NEXT_PUBLIC_SERVER}users/findBy/email`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ email }),
-    }
-  );
-
-  const responseOfIsValidName = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER}users/findBy/name`,
-    {
+    }),
+    fetch(`${process.env.NEXT_PUBLIC_SERVER}users/findBy/name`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ name }),
-    }
-  );
+    }),
+  ]);
 
-  const resultOfIsValidEmail = await responseOfIsValidEmail.json();
+  const [resultOfIsValidEmail, resultOfIsValidName] = await Promise.all([
+    responseOfIsValidEmail.json(),
+    responseOfIsValidName.json(),
+  ]);
   console.log(
-    "🚀 ~ file: createUser.ts:33 ~ createUser ~ resultOfIsValidEmail:",
+    "🚀 ~ file: createUser.ts:39 ~ createUser ~ resultOfIsValidEmail:",
     resultOfIsValidEmail
   );
-  const resultOfIsValidName = await responseOfIsValidName.json();
   console.log(
-    "🚀 ~ file: createUser.ts:35 ~ createUser ~ resultOfIsValidName:",
+    "🚀 ~ file: createUser.ts:36 ~ createUser ~ resultOfIsValidName:",
     resultOfIsValidName
   );
 
-  // const responseUsername = await fetch(
-  //   `${process.env.NEXT_PUBLIC_SERVER}users/findBy/username`,
-  //   {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({ name }),
-  //   }
-  // );
-  // const result = await response.json();
-  //   console.log("🚀 ~ file: page.tsx:17 ~ create ~ result:", result);
   // email check || username check from db
 
   // password === confirm_password check
