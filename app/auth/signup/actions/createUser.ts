11ce7c21911ce51068ad42f2484e854c3d587787
zerozoo <zerozoo385@gmail.com>
2023-10-10 22:48:23 +0900

    add create User api and add validate

--- app/auth/signup/actions/createUser.ts
+++ app/auth/signup/actions/createUser.ts
@@ -1,9 +1,12 @@
 "use server";
+
+import { Result } from "../../../dashboard/post/actions/postArticle.action";
+
 export async function createUser(formData: FormData) {
   const name = formData.get("name");
   const email = formData.get("email");
-  //   const password = formData.get("password");
-  //   const confirm_password = formData.get("confirm_password");
+  const password = formData.get("password");
+  const confirm_password = formData.get("confirm_password");
 
   const [responseOfIsValidEmail, responseOfIsValidName] = await Promise.all([
     fetch(`${process.env.NEXT_PUBLIC_SERVER}users/findBy/email`, {
@@ -22,22 +25,22 @@ export async function createUser(formData: FormData) {
     }),
   ]);
 
-  const [resultOfIsValidEmail, resultOfIsValidName] = await Promise.all([
+  const [resultOfIsValidEmail, resultOfIsValidName]: [
+    Result<any[]>,
+    Result<any[]>
+  ] = await Promise.all([
     responseOfIsValidEmail.json(),
     responseOfIsValidName.json(),
   ]);
-  console.log(
-    "🚀 ~ file: createUser.ts:39 ~ createUser ~ resultOfIsValidEmail:",
-    resultOfIsValidEmail
-  );
-  console.log(
-    "🚀 ~ file: createUser.ts:36 ~ createUser ~ resultOfIsValidName:",
-    resultOfIsValidName
-  );
 
-  // email check || username check from db
-
-  // password === confirm_password check
-
-  // save db
+  if (
+    resultOfIsValidEmail.data.length === 0 &&
+    resultOfIsValidName.data.length === 0 &&
+    password === confirm_password
+  ) {
+    console.log("ho ye! you can do this");
+    return true;
+  } else {
+    return false;
+  }
 }
